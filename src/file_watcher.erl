d1e9561ab1e939eba4a6ec667db47626268f6dfd
ppp <104762988+ppp3ppj@users.noreply.github.com>
2025-11-16 14:59:34 +0700

    Pattern matching
    * Add pattern matching to detect file change
    
    * Update todo pattern matching

--- src/file_watcher.erl
+++ src/file_watcher.erl
@@ -1,19 +1,16 @@
 -module(file_watcher).
--export([start_watching/1, get_events/1, stop_watching/1]).
+-export([start_watching/1, start_watching_with_patterns/2, get_events/1, stop_watching/1]).
 -on_load(init/0).
 
-%% This function loads the Rust NIF when the module is loaded
 init() ->
     PrivDir = case code:priv_dir(glwatch) of
         {error, _} ->
-            % Development mode: look relative to the module
             EbinDir = filename:dirname(code:which(?MODULE)),
             AppDir = filename:dirname(EbinDir),
             filename:join([AppDir, "priv"]);
         Dir ->
             Dir
     end,
-    % Load the NIF (without .so extension - Erlang adds it automatically)
     SoName = filename:join(PrivDir, "rust_watcher"),
     io:format("[Erlang] Loading NIF from: ~s~n", [SoName]),
     case erlang:load_nif(SoName, 0) of
@@ -28,8 +25,12 @@ init() ->
             {error, Reason}
     end.
 
-%% NIF stubs - these will be replaced by Rust implementations when NIF loads
+%% Start watching without patterns (watch everything)
 start_watching(_Directory) ->
+    erlang:nif_error(nif_not_loaded).
+
+%% Start watching with glob patterns
+start_watching_with_patterns(_Directory, _Patterns) ->
     erlang:nif_error(nif_not_loaded).
 
 get_events(_WatcherRef) ->
